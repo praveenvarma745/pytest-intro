2b4f0275504283ceeffe60e706e6082dbb1a7bd6
praveenvarma745 <75783586+praveenvarma745@users.noreply.github.com>
2023-08-22 12:57:53 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+import os
+
+#This is a comment.
+
+print("Hello, World!")
+
+#x variable missed for testing in line 8
+ = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+print(type(x), x)
+print(type(y), y)
+print(type(z), z)
+
+x = 5
+y = "Pvn"
+print(type(x))
+print(type(y))
