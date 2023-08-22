f83f5e73dda5035431cc24f492cac6cd547b3c03
praveenvarma745 <75783586+praveenvarma745@users.noreply.github.com>
2023-08-22 13:00:04 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import os
 print("Hello, World!")
 
 #x variable missed for testing in line 8
- = str(3)    # x will be '3'
+x = str(3)    # x will be '3'
 y = int(3)    # y will be 3
 z = float(3)  # z will be 3.0
 print(type(x), x)
